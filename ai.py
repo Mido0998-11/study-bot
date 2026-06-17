b73095f86b772d029913e2f5129493e8cdff2572
MOHAMMED ADEL <ffjjddsskkjj78@gmail.com>
2026-06-17 19:08:18 +0200

    Create ai.py

---
+++ ai.py
@@ -0,0 +1,35 @@
+import requests
+
+def ask_ai(question):
+    url = "https://api-inference.huggingface.co/models/google/flan-t5-base"
+
+    headers = {
+        "Authorization": "Bearer YOUR_HF_TOKEN"
+    }
+
+    payload = {
+        "inputs": question
+    }
+
+    try:
+        res = requests.post(url, headers=headers, json=payload)
+        return res.json()[0]["generated_text"]
+    except:
+        return "AI غير متاح حالياً"
+
+def summarize_text(text):
+    url = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+
+    headers = {
+        "Authorization": "Bearer YOUR_HF_TOKEN"
+    }
+
+    payload = {
+        "inputs": text[:4000]
+    }
+
+    try:
+        res = requests.post(url, headers=headers, json=payload)
+        return res.json()[0]["summary_text"]
+    except:
+        return "فشل التلخيص"
